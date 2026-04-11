a1c59fc182f2a5ca2cac2284f38fdbe6a805710b
Andreas Eicher <philiagus@philiagus.de>
2026-04-11 10:33:45 +0200

    wip: rename #[Instance] to #[Inject]

--- src/CommandWorker.php
+++ src/CommandWorker.php
@@ -5,18 +5,21 @@ namespace Philiagus\Figment\Cli;
 
 use Philiagus\Figment\Cli\Command\Configuration;
 use Philiagus\Figment\Cli\Contract\Command;
-use Philiagus\Figment\Container\Attribute\Instance;
+use Philiagus\Figment\Container\Attribute\Inject;
 use Philiagus\Figment\Container\Contract\InstanceList;
 use Philiagus\Figment\Container\EmptyInstanceList;
 
-readonly class CommandWorker {
+readonly class CommandWorker
+{
 
     /**
      * @param InstanceList $commands
      */
     public function __construct(
-        #[Instance('figment.cli.commands', new EmptyInstanceList())] private InstanceList $commands
-    ) {}
+        #[Inject('figment.cli.commands')] private InstanceList $commands = new EmptyInstanceList()
+    )
+    {
+    }
 
     public function work(Contract\Terminal $terminal): int
     {
@@ -28,16 +31,17 @@ readonly class CommandWorker {
                 $config = new Configuration($command);
                 $commandConfigurations[] = $config;
                 $result = $config->invoke($terminal);
-                if($result !== false) {
+                if ($result !== false) {
                     return $result;
                 }
             }
         } catch (\Throwable $e) {
-            $terminal->stderr()->print((string)$e);
+            $terminal->stderr()->print((string) $e);
         }
 
-        foreach($commandConfigurations as $configuration) {
+        foreach ($commandConfigurations as $configuration) {
             $terminal->stdout()->println($configuration->getName());
+
             return 255;
         }
 
